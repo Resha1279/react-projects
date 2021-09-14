93efd12f198f4417dfb3dbc87331f00106a1fde7
Resha Adhikari <resha.adhikari1279@gmail.com>
2021-09-14 10:15:16 +0545

    accordion

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [isShowing, setIsShowing] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button
+          className="btn"
+          onClick={() => {
+            setIsShowing(!isShowing);
+          }}
+        >
+          {isShowing ? (
+            <AiOutlineMinus></AiOutlineMinus>
+          ) : (
+            <AiOutlinePlus></AiOutlinePlus>
+          )}
+        </button>
+      </header>
+      {isShowing && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
